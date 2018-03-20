be8e69decc10dbe1573b81a438a7d6729181fc3f
Paweł Czarnecki <pawel.r.czarnecki@gmail.com>
2018-03-20 21:52:17 +0100

    Fixed indent

--- app/api.py
+++ app/api.py
@@ -8,5 +8,5 @@ def hello():
     return "Hello World from API"
 
 if __name__ == "__main__":
-	app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host='0.0.0.0', debug=True, port=8080)
